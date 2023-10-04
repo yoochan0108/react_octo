d9452a98e72e4004fa0e41579f12c43640679603
yoochan0108 <yoochan0108@naver.com>
2023-10-04 10:35:05 +0900

    useEffect 컴포넌트 마운트시 한번만 외부테이더 fetching

--- src/components/sub/department/Department.jsx
+++ src/components/sub/department/Department.jsx
@@ -1,11 +1,21 @@
+import { useEffect } from 'react';
 import Layout from '../../common/layout/Layout';
-//import styles from './Department.module.scss';
-//import clsx from 'clsx';
 
 export default function Department() {
+	useEffect(() => {
+		//해당 useEffect구문은 컴포넌트 마운트시 한번만 동작됨
+		fetch('/DB/department.json').then((data) => data.json().then((json) => {}));
+	}, []);
 	return (
 		<Layout title={'Department'}>
 			<p>디파트먼트 페이지입니다.</p>
 		</Layout>
 	);
 }
+
+/*
+  1.외부데이터를 fatch를 통해서 가져오는 법
+	2. useEffect를 이용해서 컴포넌트를 마운트시 한번만 외부데이터 fetching
+	3. 받아온 외부데이터는 useState를 통해서 state에 담아주고, State변경에 따라 화면 렌더링
+	4. state에 담겨있는 데이터로 map으로 반복만들면서 동적으로 돔생성 (JSX)
+*/
