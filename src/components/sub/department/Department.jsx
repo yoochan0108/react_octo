d74af99677dcead0380c87a177da9b17b47cc1d7
yoochan0108 <yoochan0108@naver.com>
2023-10-04 11:08:52 +0900

    맴버 데이터 출력

--- src/components/sub/department/Department.jsx
+++ src/components/sub/department/Department.jsx
@@ -1,24 +1,29 @@
 import { useEffect, useState } from 'react';
 import Layout from '../../common/layout/Layout';
+const path = process.env.PUBLIC_URL;
 
 export default function Department() {
-	console.log('re-rander');
-	const [Department, setDepartment] = useState();
+	console.log('re-render');
+	const [Department, setDepartment] = useState([]);
 	useEffect(() => {
 		//해당 useEffect구문은 컴포넌트 마운트시 한번만 동작됨
-		fetch('/DB/department.json').then((data) =>
-			data.json().then((json) => {
+		fetch(`${path}/DB/department.json`)
+			.then((data) => data.json())
+			.then((json) => {
 				console.log(json.members);
 				setDepartment(json.members);
-			})
-		);
+			});
 	}, []);
 	return (
 		<Layout title={'Department'}>
 			{Department.map((member, idx) => {
 				return (
 					<article key={idx}>
+						<div className='pic'>
+							<img src={`/img/${member.pic}`} alt={member.name} />
+						</div>
 						<h2>{member.name}</h2>
+						<p>{member.position}</p>
 					</article>
 				);
 			})}
