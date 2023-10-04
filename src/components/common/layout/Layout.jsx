395ed50039ba1ab0d3a99d0872c149f5e4d180a3
yoochan0108 <yoochan0108@naver.com>
2023-10-04 14:30:42 +0900

    페이지 변경시마다 layout컴포넌트에 on클래스를 추가해서 페이지 전환모션 적용

--- src/components/common/layout/Layout.jsx
+++ src/components/common/layout/Layout.jsx
@@ -1,9 +1,21 @@
 import styles from './Layout.module.scss';
 import clsx from 'clsx';
+import { useEffect, useRef } from 'react';
 
 export default function Layout({ title, children, styleName }) {
+	//가상돔 요소를 document.qurrySelector를 쓰지말고 실시간으로 참조하고 싶을 떼
+	//빈 참조갹체를 useRef로 생성
+	const frame = useRef(null);
+	const isOn = useRef(false);
+
+	useEffect(() => {
+		console.log('page변경');
+		//컴포넌트 마운트시 한번만 호출
+		//페이지가 변경이 되서 각 페이지마다의 layout컴포넌트가 마운트될시 한번만 on클래스를 추가
+		isOn.current = true;
+	}, []);
 	return (
-		<section className={clsx(styles.layout, styleName)}>
+		<section ref={frame} className={clsx(styles.layout, styleName, isOn.current ? styles.on : '')}>
 			<figure></figure>
 
 			<div className={clsx(styles.content)}>
--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -1,10 +1,11 @@
 import Layout from '../../common/layout/Layout';
+import styles from './Gallery.module.scss';
 //import styles from './Department.module.scss';
 //import clsx from 'clsx';
 
 export default function Gallery() {
 	return (
-		<Layout title={'Gallery'}>
+		<Layout title={'Gallery'} styleName={styles.gallery}>
 			<p>갤러리 페이지입니다.</p>
 		</Layout>
 	);
