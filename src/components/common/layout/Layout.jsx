2886daa137d4f29cfa9a013f0127a85c63fce51b
yoochan0108 <yoochan0108@naver.com>
2023-10-04 14:52:45 +0900

    레이아웃 제목 텍스트 분리

--- src/components/common/layout/Layout.jsx
+++ src/components/common/layout/Layout.jsx
@@ -3,32 +3,37 @@ import clsx from 'clsx';
 import { useEffect, useRef, useState } from 'react';
 
 export default function Layout({ title, children, styleName }) {
-	//컴포넌트 마운트시 변경할 state추가
 	const [IsOn, setIsOn] = useState(false);
-	//가상돔 요소를 document.qurrySelector를 쓰지말고 실시간으로 참조하고 싶을 떼
-	//빈 참조갹체를 useRef로 생성
 	const frame = useRef(null);
-	const isOn = useRef(false);
+	const tit = useRef(null);
+
+	//인수로 참조객체를 받아서 참조객체의 글자값을 반복돌면서 span으로 감싸주는 함수
+	const splitText = (ref) => {
+		//문자열이 담길 변수 초기화
+		let tags = '';
+
+		//참조객체안쪽의 글자를 innerText로 구해서
+		//for of로 반복처리하면서 span태그로 감싸줌
+		for (let letter of ref.current.innerText) {
+			tags += `<span>${letter}</span>`;
+		}
+		//기존 참조객체 안쪽의 글자는 지워주고
+		ref.current.innerText = '';
+		//span으로 감싸준 문자열을 참조객체의 innerHTML으로 삽입
+		ref.current.innerHTML = tags;
+	};
 
 	useEffect(() => {
-		console.log('page변경');
-		//컴포넌트 마운트시 한번만 호출
-		//컴포넌트 마운트시 IsOn값을 true로 변경
-		//IsOn을 useRef가 아닌 state로 변경해야 되는 이유
-		//useRef값을 변경해도 리액트는 변경점을 인지 못해서 재렌더링이 안되기 때문에
 		setIsOn(true);
+		//컴포넌트가 마운트되자마자 h1요소가 담겨있는 참조객체를 인수로 전달해서 글자 분리
+		splitText(tit);
 	}, []);
 	return (
-		//참조하고싶은 가상돔 요소에 ref로 연결
-		<section
-			ref={frame}
-			//IsOn State가 true 일떄에만 on 클래스명 적용
-			className={clsx(styles.layout, styleName, IsOn ? styles.on : '')}
-		>
+		<section ref={frame} className={clsx(styles.layout, styleName, IsOn ? styles.on : '')}>
 			<figure></figure>
 
 			<div className={clsx(styles.content)}>
-				<h1>{title}</h1>
+				<h1 ref={tit}>{title}</h1>
 				{children}
 			</div>
 		</section>
